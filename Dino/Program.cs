ad7630649a1ea8ea6a3ebf0ce39e9a15a88bfa97
Grasa1 <gulyas.gergely22k@egressy.info>
2026-03-27 09:15:00 +0100

    Refactor Dino program and fix syntax errors

--- Dino/Program.cs
+++ Dino/Program.cs
@@ -1,8 +1,10 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-
-namespace Dino
+namespace dino2
 {
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+
     internal class Program
     {
         static void Main(string[] args)
@@ -17,7 +19,7 @@ namespace Dino
 
             //1 Dinok szama
             int lista = 0;
-            foreach(Dino item in dinok)
+            foreach (Dino item in dinok)
             {
                 lista++;
             }
@@ -26,10 +28,10 @@ namespace Dino
 
 
             //2 Legidősebb dino
-            Dino legidosebb = dinok[0]; 
+            Dino legidosebb = dinok[0];
             foreach (Dino item in dinok)
             {
-                if(legidosebb.age < item.age)
+                if (legidosebb.age < item.age)
                 {
                     legidosebb = item;
                 }
@@ -37,7 +39,7 @@ namespace Dino
             Console.WriteLine($" A lgidősebb dino a {legidosebb}");
             Console.WriteLine("------------------------------------------------");
 
-            for (int i = 0; i < dinok.Count- 1; i++)
+            for (int i = 0; i < dinok.Count - 1; i++)
             {
                 for (int j = 0; j < dinok.Count - 1 - i; j++)
                 {
@@ -49,56 +51,129 @@ namespace Dino
                     }
                 }
             }
-            Console.writeLine("kor szerint")
-                foreach(Dino item in dinok)
-                {
-                    Console.WriteLine(item.ToString())
-                }
-             for (int i = 0; i < dinok.Count- 1; i++)
+            Console.WriteLine("kor szerint");
+            foreach (Dino item in dinok)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            for (int i = 0; i < dinok.Count - 1; i++)
             {
                 for (int j = 0; j < dinok.Count - 1 - i; j++)
                 {
-                    if (string.Compare(dinok[j].name, dinok[j + 1].name))
+                    if (string.Compare(dinok[j].Name, dinok[j + 1].Name))
                     {
                         Dino temp = dinok[j];
                         dinok[j] = dinok[j + 1];
                         dinok[j + 1] = temp;
                     }
-                
+
                 }
             }
-            Console.WriteLine("Név szerint")
-                foreach(Dino item in dinok)
-                {
-                    Console.WriteLine(item.ToString())
-                }
-            Dino nehezebb = dinok[0]; 
+            Console.WriteLine("Név szerint");
             foreach (Dino item in dinok)
             {
-                if(legidosebb.weight < item.weight)
+                Console.WriteLine(item.ToString());
+            }
+            Dino nehezebb = dinok[0];
+            foreach (Dino item in dinok)
+            {
+                if (legidosebb.weight < item.weight)
                 {
                     nehezebb = item;
                 }
             }
-            Console.WriteLine($" A legnehezebb dino a {nehezebb.name}");
+            Console.WriteLine($" A legnehezebb dino a {nehezebb.Name}");
             Console.WriteLine("------------------------------------------------");
-            List<Dino> Tdinos  = new LIst<Dino>();
+            List<Dino> Tdinos = new List<Dino>();
             foreach (Dino item in dinok)
             {
-                if(item.type.StartsWith("T") && !Tdinos.COntains(item))
+                if (item.type.StartsWith("T") && !Tdinos.Contains(item))
                 {
-                    Tdinos.Add(iem)
+                    Tdinos.Add(item);
                 }
             }
-            Console.WriteLine("T vel kezd")
-                foreach(Dino item in Tdinos)
+            Console.WriteLine("T vel kezd");
+            foreach (Dino item in Tdinos)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            //DInok átlagsúlya
+            double atlag = 0;
+            foreach (Dino item in dinok)
+            {
+
+                atlag += item.weight;
+
+            }
+            Console.WriteLine($"A dinok átlag súlya: {atlag / lista}");
+
+            // piros színű dinok kiírása
+            foreach (Dino item in dinok)
+            {
+                if (item.color == "piros")
                 {
-                    Console.WriteLine(item.ToString())
+                    Console.WriteLine($"A piros színű dino neve: {item.Name}");
                 }
-            
-        
+            }
+            //legkönyebb dino kiírása
+            Dino legkonnyebb = dinok[0];
+            foreach (Dino item in dinok)
+            {
+                if (legkonnyebb.weight > item.weight)
+                {
+                    legkonnyebb = item;
+                }
+            }
 
+            Console.WriteLine($"A legkönnyebb dino a {legkonnyebb.Name}");
+            // 4 és 6 éves dinok kiírása
+            foreach (Dino item in dinok)
+            {
+                if (item.age == 4 || item.age == 6)
+                {
+                    Console.WriteLine($"A 4 és 6 éves dino neve: {item.Name}");
+                }
+            }
+            // 1000 nél könnyebb dinok kiírása
+            foreach (Dino item in dinok)
+            {
+                if (item.weight < 1000)
+                {
+                    Console.WriteLine($"A 1000-nél könnyebb dino neve: {item.Name}");
+                }
+            }
+            //dinok száma amik nem adoptáltak
+            int nemadoptalt = 0;
+            foreach (Dino item in dinok)
+            {
+                if (!item.adopted)
+                {
+                    nemadoptalt++;
+                }
+            }
+            Console.WriteLine($"A nem adoptált dinok száma: {nemadoptalt}");
+            //összes dinok átlag életkora
+            double atlageletkor = 0;
+            foreach (Dino item in dinok)
+            {
+                atlageletkor += item.age;
+            }
+            Console.WriteLine($"A dinok átlag életkora: {atlageletkor / lista}");
+            //legfiatalabb 3 dino kiirása
+            List<Dino> legfiatalabb = new List<Dino>();
+            for (int i = 0; i < 3; i++)
+            {
+                legfiatalabb.Add(dinok[i]);
+            }
+            Console.WriteLine("A legfiatalabb 3 dino:");
+
+
+
+
+
+
+        }
     }
-
-    
 }
+
