b46990ed5faa7420964660dd0273ca8e99d456dc
Grasa1 <gulyas.gergely22k@egressy.info>
2026-03-27 08:30:48 +0100

    Implement sorting and filtering for Dino objects
    Added sorting and filtering functionality for Dino list.

--- Dino/Program.cs
+++ Dino/Program.cs
@@ -49,7 +49,53 @@ namespace Dino
                     }
                 }
             }
-        }
+            Console.writeLine("kor szerint")
+                foreach(Dino item in dinok)
+                {
+                    Console.WriteLine(item.ToString())
+                }
+             for (int i = 0; i < dinok.Count- 1; i++)
+            {
+                for (int j = 0; j < dinok.Count - 1 - i; j++)
+                {
+                    if (string.Compare(dinok[j].name, dinok[j + 1].name))
+                    {
+                        Dino temp = dinok[j];
+                        dinok[j] = dinok[j + 1];
+                        dinok[j + 1] = temp;
+                    }
+                
+                }
+            }
+            Console.WriteLine("Név szerint")
+                foreach(Dino item in dinok)
+                {
+                    Console.WriteLine(item.ToString())
+                }
+            Dino nehezebb = dinok[0]; 
+            foreach (Dino item in dinok)
+            {
+                if(legidosebb.weight < item.weight)
+                {
+                    nehezebb = item;
+                }
+            }
+            Console.WriteLine($" A legnehezebb dino a {nehezebb.name}");
+            Console.WriteLine("------------------------------------------------");
+            List<Dino> Tdinos  = new LIst<Dino>();
+            foreach (Dino item in dinok)
+            {
+                if(item.type.StartsWith("T") && !Tdinos.COntains(item))
+                {
+                    Tdinos.Add(iem)
+                }
+            }
+            Console.WriteLine("T vel kezd")
+                foreach(Dino item in Tdinos)
+                {
+                    Console.WriteLine(item.ToString())
+                }
+            
         
 
     }
